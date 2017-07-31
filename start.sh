265b1d4bf65d4cb12770be507a86a6fd66b2466c
andrewlarsen <andrew.larsen@concordusa.com>
2017-07-30 20:26:31 -0500

    add initialize

--- start.sh
+++ start.sh
@@ -30,6 +30,7 @@ if [ ! -f /var/www/html/moodle/config.php ]; then
   mysqladmin -u root password $MYSQL_PASSWORD
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
   mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE moodle; GRANT ALL PRIVILEGES ON moodle.* TO 'moodle'@'localhost' IDENTIFIED BY '$MOODLE_PASSWORD'; FLUSH PRIVILEGES;"
+  mysqld --initialize
   killall mysqld
 fi
 # start all the services
