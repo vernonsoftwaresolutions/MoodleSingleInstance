8414d46bb248c70c8b040b41aae0c776d174714a
andrewlarsen <andrew.larsen@concordusa.com>
2017-08-30 20:14:51 -0500

    add deployment and test for both templates

---
+++ cleanup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ecstemplate=$1
+moodle1=$2
+moodle2=$3
+
+aws cloudformation delete-stack --stack-name "${ecstemplate}"
+aws cloudformation delete-stack --stack-name "${moodel1}"
+aws cloudformation delete-stack --stack-name "${moodel2}"
