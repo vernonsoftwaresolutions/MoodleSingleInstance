a5d7c801a7bac60da98c79a0b7ba5a0f881a8a22
andrewlarsen <andrew.larsen@concordusa.com>
2017-09-08 20:11:35 -0500

    don't remove test

--- cloudformation_test.sh
+++ cloudformation_test.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 ecstemplate=$1
 moodle1=$2
 moodle2=$3
@@ -21,8 +23,11 @@ alblistener=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --
 aws cloudformation package --template-file cloudformation/moodle_tenant.yml --output-template-file moodletenant-output.yml --s3-bucket circleci.deployables
 aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle1}" --parameter-overrides VpcId=vpc-c7aa77be Priority=1 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}" ecslbdnsname="${ecslbdnsname}"	 ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}" HostedZoneName=vssdevelopment.com  ClientName=brad
 #just send basic test
+sleep 15
 curl http://brad.vssdevelopment.com/
 aws cloudformation package --template-file cloudformation/moodle_tenant.yml --output-template-file moodletenant-output.yml --s3-bucket circleci.deployables
 aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle2}" --parameter-overrides VpcId=vpc-c7aa77be Priority=2 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}"	ecslbdnsname="${ecslbdnsname}" ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}"  HostedZoneName=vssdevelopment.com  ClientName=alec
 #just send basic test
+sleep 15
+
 curl http://alec.vssdevelopment.com/
