3d275e748ff4fbfa6eee30897f76f7f11a033d7b
andrewlarsen <andrew.larsen@concordusa.com>
2017-09-19 19:36:01 -0500

    add output url

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,6 @@ ecstemplate=$1
 moodle1=$2
 moodle2=$3
 
-aws cloudformation delete-stack --stack-name "${moodel1}"
-aws cloudformation delete-stack --stack-name "${moodel2}"
+aws cloudformation delete-stack --stack-name "${moodle1}"
+aws cloudformation delete-stack --stack-name "${moodle2}"
 #aws cloudformation delete-stack --stack-name "${ecstemplate}"
