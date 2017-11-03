01c18dacd2db991a0c18b25fdb2a52bfaa47f07e
andrewlarsen <andrew.larsen@concordusa.com>
2017-11-02 20:35:35 -0500

    fix parameter

--- cloudformation_test.sh
+++ cloudformation_test.sh
@@ -11,7 +11,7 @@ aws cloudformation validate-template --template-body file://cloudformation/ecs.y
 aws cloudformation validate-template --template-body file://cloudformation/moodle_tenant.yml
 #package and deploy the ecs cluster
 aws cloudformation package --template-file cloudformation/ecs.yml --output-template-file ecs-output.yml --s3-bucket circleci.deployables
-aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName=dummy_key1 VpcId=vpc-c7aa77be SubnetId=subnet-b61d81fe,subnet-0202dc58 InstanceType=t2.medium 
+aws cloudformation deploy --template-file ecs-output.yml --capabilities CAPABILITY_IAM --stack-name "${ecstemplate}" --parameter-overrides KeyName=dummy_key1 VpcId=vpc-c7aa77be SubnetId=subnet-b61d81fe,subnet-0202dc58 InstanceType=t2.medium DomainName=vssdevelopment.com
 #Get output values, this is a soemwhat naive approach since it is a lot of api calls
 ecscluster=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'ecscluster')]][0][*].{OutputValue:OutputValue}" --output text`
 ecslbarn=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --query "Stacks[0].[Outputs[? starts_with(OutputKey, 'ecslbarn')]][0][*].{OutputValue:OutputValue}" --output text`
@@ -21,12 +21,12 @@ alblistener=`aws cloudformation describe-stacks --stack-name "${ecstemplate}" --
 
 #package and deploy an instance
 aws cloudformation package --template-file cloudformation/moodle_tenant.yml --output-template-file moodletenant-output.yml --s3-bucket circleci.deployables
-aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle1}" --parameter-overrides VpcId=vpc-c7aa77be Priority=1 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}" ecslbdnsname="${ecslbdnsname}"	 ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}" HostedZoneName=vssdevelopment.com  ClientName=brad DomainName=vssdevelopment.com
+aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle1}" --parameter-overrides VpcId=vpc-c7aa77be Priority=1 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}" ecslbdnsname="${ecslbdnsname}"	 ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}" HostedZoneName=vssdevelopment.com  ClientName=brad 
 #just send basic test
 sleep 15
 curl https://brad.vssdevelopment.com/
 aws cloudformation package --template-file cloudformation/moodle_tenant.yml --output-template-file moodletenant-output.yml --s3-bucket circleci.deployables
-aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle2}" --parameter-overrides VpcId=vpc-c7aa77be Priority=2 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}"	ecslbdnsname="${ecslbdnsname}" ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}"  HostedZoneName=vssdevelopment.com  ClientName=alec DomainName=vssdevelopment.com
+aws cloudformation deploy --template-file moodletenant-output.yml --capabilities CAPABILITY_IAM --stack-name "${moodle2}" --parameter-overrides VpcId=vpc-c7aa77be Priority=2 ecscluster="${ecscluster}" ecslbarn="${ecslbarn}"	ecslbdnsname="${ecslbdnsname}" ecslbhostedzoneid="${ecslbhostedzoneid}" alblistener="${alblistener}"  HostedZoneName=vssdevelopment.com  ClientName=alec 
 #just send basic test
 sleep 15
 
